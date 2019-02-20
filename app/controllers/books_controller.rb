17bbb6a4c9deffcf28cd4d44c686f44a6b20e632
Alexandra <alexandra.schachtner@gmail.com>
2019-02-19 18:50:01 -0600

    thats all folks, use this as a refrence for you final project

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,4 +26,22 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit!
+  end
 end
